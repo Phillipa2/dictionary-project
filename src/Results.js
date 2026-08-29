01d4450b2f1641a8c71c729711225550fe032c88
Phillipa2 <phillipaatieno7@gmail.com>
2026-08-28 16:35:55 -0800

    Rendered the word meanings by use of map

---
+++ src/Results.js
@@ -0,0 +1,17 @@
+import React  from "react";
+import Meaning from "./Meaning";
+
+export default function Result(props) {
+    if (props.results) {
+        return (
+            <div className="Results">
+               <h2>{props.results.word}</h2>
+               {props.results.meanings.map(function (meaning, index) {
+                   return <Meaning key={index} meaning={meaning} />;
+                })}
+            </div>
+        );
+    } else {
+        return null;
+    }
+}
