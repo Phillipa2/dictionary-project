964c79ba9d84c75f92284b7c298fdef522e088f1
Phillipa2 <phillipaatieno7@gmail.com>
2026-08-30 16:06:08 -0800

    Created a synonym component to display synonyms if available

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     return (
         <div className="Meaning">
             <h3>{props.meaning.partOfSpeech}</h3>
                 <p>
-                    {props.meaning.definition}
+                    <strong>Definition: </strong>{props.meaning.definition}
                     <br />
-                    <em>{props.meaning.example}</em>
+                    <strong>Example: </strong><em>{props.meaning.example}</em>
+                    <br />
+                    <Synonyms synonyms={props.meaning.synonyms} />
                 </p>
         </div>
     );
